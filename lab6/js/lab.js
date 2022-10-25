e15fbb0f3cd89cbdfac481e944a0e276afbd4216
Leslie <leslie.horwitz@gmail.com>
2022-10-25 08:11:12 -0700

    Update lab.js

---
+++ lab6/js/lab.js
@@ -0,0 +1,23 @@
+/*
+*Author: Wes Modes <wmodes@csumb.edu>
+*created: 13 september
+*License: Public Domain
+*/
+
+// Define Variables
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
+
+//calcualate
+age = 2019-year;
+
+}
+{
+//Output
+document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+
+
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
